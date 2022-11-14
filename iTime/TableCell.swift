ec5687aa8528af5f01b1c3e40109e9be621ccb51
KseniaAnisimova <ksyusha_g@mail.ru>
2022-11-14 15:29:50 +0300

    first commit

---
+++ iTime/TableCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableCell.swift
+//  iTime
+//
+//  Created by user on 14.11.2022.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    @IBOutlet weak var tableLabel: UILabel!
+}
